583ae64e79befe72495db660056b173f33cf7aca
MohammadP2020 <72582740+MohammadP2020@users.noreply.github.com>
2020-10-14 20:10:52 +1100

    Create get_seqs.py

---
+++ get_seqs.py
@@ -0,0 +1,15 @@
+from Bio import SeqIO
+import sys
+#1. file with IDs
+
+s_d=set()
+
+for l in open(sys.argv[1]):
+    s_d.add(l.rstrip())
+
+for seq in SeqIO.parse(sys.argv[2], "fasta"):
+    g0=seq.id.split(".")
+    g=".".join(g0[0:-1])
+    if(g in s_d):
+        sys.stdout.write(seq.format("fasta"))
+        sys.stdout.flush()
